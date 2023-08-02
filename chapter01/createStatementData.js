9921ed68773270d3e3c2600389b11a408ef084c2
전지현 <jihyeon.jeon@medistream.co.kr>
2023-08-02 21:37:05 +0900

    02-39.팩터리 함수를 서브클래스 사용에 맞게 로직 추가

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -39,8 +39,20 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  // return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
 
 export default function createStatementData(invoice, plays) {
   // 중간 데이터 생성을 전담하는 함수
