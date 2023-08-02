5d2ab6c239b0f59e4e195fa24644b11dcd117c05
전지현 <jihyeon.jeon@medistream.co.kr>
2023-08-02 21:21:14 +0900

    02-35.amountFor를 사용하던 부분을 클래스를 사용하도록 수정한다

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -42,7 +42,7 @@ export default function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance)); // 공연료 계산기 생성
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = calculator.play; // 중간 데이터에 연극 정보를 저장
-    result.amount = amountFor(result); // 중간 데이터에 비용을 계산해 저장
+    result.amount = calculator.amount; // 중간 데이터에 비용을 계산해 저장
     result.volumeCredits = volumeCreditsFor(result); // 중간 데이터에 포인트를 계산해 저장
     return result;
   }
@@ -50,29 +50,6 @@ export default function createStatementData(invoice, plays) {
   function playFor(aPerformance) {
     // renderPlaintText()의 중첩 함수였던 playFor()를 statement()로 옮김
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount; // 원본 함수인 amountFor()도 계산기 이용하도록 수정
-    // let result = 0; // 변수 초기화
-    // switch (aPerformance.play.type) {
-    //   case 'tragedy':
-    //     result = 40000;
-    //     if (aPerformance.audience > 30) {
-    //       result += 1000 * (aPerformance.audience - 30);
-    //     }
-    //     break;
-    //   case 'comedy':
-    //     result = 30000;
-    //     if (aPerformance.audience > 20) {
-    //       result += 10000 + 500 * (aPerformance.audience - 20);
-    //     }
-    //     result += 300 * aPerformance.audience;
-    //     break;
-    //   default:
-    //     throw new Error(`unknown type: ${aPerformance.play.type}`);
-    // }
-    // return result;
   }
 
   function volumeCreditsFor(aPerformance) {
